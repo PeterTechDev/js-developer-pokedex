63addfe3d9b73e10fe0ba08672823b185d8ca6d7
petertechdev <peterleite.dev@gmail.com>
2023-09-05 06:03:12 -0300

    Adiciona feature que mostra mais detalhes do pokemon em uma modal quando o user clicar

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ function convertPokemonToLi(pokemon) {
                 </ol>
 
                 <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                    alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -45,3 +45,71 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+// Adicione a função getPokemonDetailByName ao seu objeto pokeApi
+pokeApi.getPokemonDetailByName = (pokemonName) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`;
+
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Não foi possível buscar o Pokémon: ${pokemonName}`);
+            }
+            return response.json();
+        })
+        .then((pokeDetail) => convertPokeApiDetailToPokemon(pokeDetail))
+        .catch((error) => {
+            console.error(error);
+            return null;
+        });
+};
+
+// MODAL
+// Adicione um evento de clique aos Pokémon na lista
+pokemonList.addEventListener('click', (event) => {
+    const pokemonElement = event.target.closest('li.pokemon');
+
+    console.log(pokemonElement);
+
+    if (pokemonElement) {
+        const pokemonName = pokemonElement.querySelector('.name').textContent;
+
+        // Use o nome do Pokémon para buscar informações detalhadas
+        pokeApi.getPokemonDetailByName(pokemonName).then((pokemonDetail) => {
+            if (pokemonDetail) {
+                // Preencha o conteúdo da modal com as informações detalhadas do Pokémon
+                console.log(pokemonDetail);
+                const pokemonInfo = document.getElementById('pokemonInfo');
+                pokemonInfo.innerHTML = `
+                    <h2>${pokemonDetail.name}</h2>
+                    <img src="${pokemonDetail.photo}" alt="${pokemonDetail.name}">
+                    <p>Types: ${pokemonDetail.types.join(', ')}</p>
+                    <!-- Adicionar outras informações detalhadas aqui -->
+                `;
+
+                // Exiba a modal
+                const modal = document.getElementById('pokemonModal');
+                modal.style.display = 'block';
+
+                const modalContent = document.querySelector('.modal-content');
+                modalContent.classList.add(pokemonDetail.type);
+            }
+        });
+    }
+    
+});
+
+// Adicione um evento de clique para fechar a modal quando clicar no botão "Fechar" (×)
+const closeModalButton = document.querySelector('.close');
+closeModalButton.addEventListener('click', () => {
+    const modal = document.getElementById('pokemonModal');
+    modal.style.display = 'none';
+
+    const modalContent = document.querySelector('.modal-content');
+
+    Array.from(modalContent.classList).forEach((className) => {
+        if (className !== 'modal-content') {
+            modalContent.classList.remove(className);
+        }
+    });
+});
